c6d0fab9924d97c89c105282174dbfba0f3bb37a
Cod-lvr <79340509+cod-lvr@users.noreply.github.com>
2021-12-08 21:25:15 +0200

    add script file and logic for tip app

--- main.js
+++ main.js
@@ -1,35 +1,47 @@
-const billAmount = document.querySelector('#bill').value;
-const numOfPeople = document.querySelector('#people').value;
-const optionsBtns = document.querySelectorAll('.options button');
-const optionsinput = document.querySelector('.options input').value;
+const optionsBtns = document.querySelectorAll(".options button");
+const optionsinput = document.querySelector(".options input").value;
+const errMessage = document.querySelector(".err-msg");
 
-const resultPerson = document.querySelector('.result-person');
-const resultTotal = document.querySelector('.result-total');
+const bill = document.getElementById("bill");
+const people = document.getElementById("numOfPeople");
 
-optionsBtns.forEach(btn => {
-    btn.addEventListener('click', () => {
-        console.log(btn.textContent);
-        calculateTip(btn.textContent);
-    })
+const resultPerson = document.querySelector(".result-person");
+const resultTotal = document.querySelector(".result-total");
+
+const resetBtn = document.querySelector(".reset");
+
+optionsBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    calculateTip(btn.textContent);
+  });
 });
 
-
 function calculateTip(tip) {
-    console.log(tip);
-    if (optionsinput === "NUMBER") {
-        tip = optionsinput;
-    }
-    // if (billAmount.length === 0 && numOfPeople.length === 0) {
-    //     return;
-    // }
-    console.log(billAmount, numOfPeople);
-    const tipAmoutPerPerson = +billAmount * +tip /100 ;
-    const tipTotalPerPerson = tipAmoutPerPerson * +numOfPeople;
-    console.log(tipAmoutPerPerson, tipTotalPerPerson);
-   addDomValue(tipTotalPerPerson, tipTotalPerPerson);
+  const billAmount = bill.value;
+  const numOfPeople = people.value;
+  if (numOfPeople === "0") {
+    people.style.border = "4px solid #ff944d";
+    errMessage.style.display = "inline-block";
+    return;
+  } else {
+    errMessage.style.display = "none";
+    people.style.border = "none";
+  }
+
+  console.log(billAmount, numOfPeople);
+  const tipAmoutPerPerson = (+billAmount * tip) / 100;
+  const tipTotalPerPerson = tipAmoutPerPerson * +numOfPeople;
+  addDomValue(tipAmoutPerPerson, tipTotalPerPerson);
 }
 
 function addDomValue(amountperson, totalPerson) {
-    resultPerson.textContent = amountperson;
-    resultTotal.textContent = totalPerson;
+  resultPerson.textContent = `$${amountperson}`;
+  resultTotal.textContent = `$${totalPerson}`;
 }
+
+resetBtn.addEventListener("click", () => {
+  bill.value = "";
+  people.value = "";
+  resultPerson.textContent = `$${0.0}`;
+  resultTotal.textContent = `$${0.0}`;
+});
